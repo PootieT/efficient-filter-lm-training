2bb963ac4a55cd0d32f948fecadd09e62fafe022
Zilu Tang <zilu.p.tang@gmail.com>
2023-05-09 23:13:47 -0400

    resolve conflict

--- experiments/finetune_roberta_on_pile.py
+++ experiments/finetune_roberta_on_pile.py
@@ -8,12 +8,15 @@ model_args = ModelArguments(
 )
 data_args = DataTrainingArguments(
     dataset_name="the_pile",
+    # remove train_file argument for baseline training
+    # train_file="../data/CMS100_CS100_TL0.1_TH0.7/filtered_data.json",
+    validation_file="../data/the_pile_valid_1000.json",
     streaming=True,
     max_seq_length=512,
     max_eval_samples=1000,
 )
 train_args = TrainingArguments(
-    run_name="baseline",
+    run_name="CMS100_CS100_TL0.1_TH0.7",
     output_dir="../dump/pile_baseline",
     logging_dir="../wandb",
     num_train_epochs=1,
@@ -24,11 +27,12 @@ train_args = TrainingArguments(
     do_train=True,
     do_eval=True,
     evaluation_strategy="steps",
-    eval_steps=100,
+    eval_steps=500,
     max_steps=10000000,
     logging_steps=25,
     seed=42,
-    overwrite_output_dir=True,
+    # overwrite_output_dir=True,
+    # resume_from_checkpoint=True,
     fp16=True,
 )
 
